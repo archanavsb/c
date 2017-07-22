e7eac448b7156c3ae6ac501476d38e5ef77d7aeb
archanavsb <30317021+archanavsb@users.noreply.github.com>
2017-07-22 14:13:43 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("character is an alphabet");
+if(ch<='a' && ch>=z)||(ch<='A' && ch>='Z')
+else
+printf("character is not an alphabet");
+return 0;
+}
