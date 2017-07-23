1051e624268eac8597fb465be63be35daa0e4235
archanavsb <30317021+archanavsb@users.noreply.github.com>
2017-07-23 11:06:14 +0530

    Create numbers.c

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,num,sum=0;
+printf("enter a positive integers\n");
+scanf("%d",&n);
+for(i=1;i<=sum;i++);
+{
+sum+=i;
+}
+printf("sum=%d",sum);
+return 0;
+}
